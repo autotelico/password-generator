513a955758e9a479fe1bbe8b6d6381a2a02899b1
Henrique Heron <henriqueheron1@gmail.com>
2024-06-03 23:53:11 -0300

    switch - and + button positions for better UX

--- app/components/MainContent.tsx
+++ app/components/MainContent.tsx
@@ -1,19 +1,18 @@
 'use client';
 import { useState } from 'react';
 import generatePassword from '../scripts/passwordGenerator';
-import { MdContentCopy } from 'react-icons/md';
 import { TbCopy, TbCopyCheck } from 'react-icons/tb';
 
-const initialPassword = generatePassword(12)
+const initialPassword = generatePassword(12);
 
 export default function MainContent(): React.ReactNode {
   const [pwLength, setPwLength] = useState(12);
   const [password, setPassword] = useState(initialPassword);
-  const [pwWasCopied, setPwWasCopied] = useState(false)
-  
+  const [pwWasCopied, setPwWasCopied] = useState(false);
+
   const passwordGenerate = () => {
     setPassword(generatePassword(pwLength));
-    setPwWasCopied(false)
+    setPwWasCopied(false);
   };
 
   const updatePwLength = (operation: string): void => {
@@ -27,18 +26,28 @@ export default function MainContent(): React.ReactNode {
   };
 
   const copyPasswordToClipboard = () => {
-    const pwInput = document.querySelector<HTMLInputElement>('#password-input')!.value
-    navigator.clipboard.writeText(pwInput)
-    setPwWasCopied(true)
-  }
+    const pwInput =
+      document.querySelector<HTMLInputElement>('#password-input')!.value;
+    navigator.clipboard.writeText(pwInput);
+    setPwWasCopied(true);
+  };
 
   return (
-    <main className='bg-slate-200 py-10'>
+    <main className="bg-slate-200 py-10">
       <div className="w-[300px] sm:w-[500px] h-[500px] sm:h-auto relative left-[50%] translate-x-[-50%]">
-        <h2 className='text-center'>Your password:</h2>
-        <div className='flex flex-col sm:flex-row justify-center items-center gap-3'>
-            <input type="text" value={password} id='password-input' className='rounded-md p-1'/>
-            {pwWasCopied ? <TbCopyCheck size={30} onClick={copyPasswordToClipboard}/> : <TbCopy size={30} onClick={copyPasswordToClipboard}/>}
+        <h2 className="text-center">Your password:</h2>
+        <div className="flex flex-col sm:flex-row justify-center items-center gap-3">
+          <input
+            type="text"
+            value={password}
+            id="password-input"
+            className="rounded-md p-1"
+          />
+          {pwWasCopied ? (
+            <TbCopyCheck size={30} onClick={copyPasswordToClipboard} />
+          ) : (
+            <TbCopy size={30} onClick={copyPasswordToClipboard} />
+          )}
         </div>
         <button
           onClick={passwordGenerate}
@@ -46,13 +55,23 @@ export default function MainContent(): React.ReactNode {
         >
           Generate
         </button>
-        <div className='flex justify-evenly gap-5'>
-            <button onClick={() => updatePwLength('+')} className='size-[50px] bg-[#242424] text-[#eee] px-4 py-2 rounded-md font-semibold'>+</button>
-            <div>
-                <p className='text-center'>Password length:</p>
-                <p className='text-center text-2xl'>{pwLength}</p>
-            </div>
-            <button onClick={() => updatePwLength('-')} className='size-[50px] bg-[#242424] text-[#eee] px-4 py-2 rounded-md font-semibold'>-</button>
+        <div className="flex justify-evenly gap-5">
+          <button
+            onClick={() => updatePwLength('-')}
+            className="size-[50px] bg-[#242424] text-[#eee] px-4 py-2 rounded-md font-semibold"
+          >
+            -
+          </button>
+          <div>
+            <p className="text-center">Password length:</p>
+            <p className="text-center text-2xl">{pwLength}</p>
+          </div>
+          <button
+            onClick={() => updatePwLength('+')}
+            className="size-[50px] bg-[#242424] text-[#eee] px-4 py-2 rounded-md font-semibold"
+          >
+            +
+          </button>
         </div>
       </div>
     </main>
