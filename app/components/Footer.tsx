70e51355b1d69503c7ff3cdc74e76bf7263eabef
Henrique Heron <henriqueheron1@gmail.com>
2024-06-04 00:10:22 -0300

    add footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,26 @@
+import { DiGithub } from "react-icons/di"
+import { LiaLinkedin } from "react-icons/lia"
+import { BsWhatsapp } from "react-icons/bs"
+import { GoMail } from "react-icons/go"
+
+export default function Footer(): React.ReactNode {
+    return (
+        <footer className="bg-[#242424] text-[#eee] py-5">
+        <div className="mx-auto text-center">
+          <p className=" text-lg">Desenvolvido por</p>
+          <p className="font-semibold text-2xl">Henrique Heron</p>
+        </div>
+        <div className="flex w-[200px] justify-between items-center mx-auto">
+          <a href="https://github.com/autotelico">
+              <DiGithub size={50} className="hover:scale-110 duration-500" />
+          </a>
+          <a href="https://www.linkedin.com/in/henrique-mag/">
+              <LiaLinkedin size={50} className="hover:scale-110 duration-500" />
+          </a>
+          <a href="mailto:henriqueheronhh@gmail.com">
+              <GoMail size={40} className="hover:scale-110 duration-500"/>
+          </a>
+        </div>
+      </footer>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 export default function Home() {
   return (
     <>
       <Header />
       <MainContent />
+      <Footer />
     </>
   );
 }
