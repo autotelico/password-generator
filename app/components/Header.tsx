a2654a3709c5ad1b9b7da53733353dd8230f924c
Henrique Heron <henriqueheron1@gmail.com>
2024-06-03 23:50:02 -0300

    initial commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header(): React.ReactNode {
+    return (
+        <header className="bg-black text-white text-4xl sm:text-6xl font-bold py-16 flex justify-center items-center text-center select-none">
+            <h1>Password Generator</h1>
+        </header>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+    </>
+  );
+}
